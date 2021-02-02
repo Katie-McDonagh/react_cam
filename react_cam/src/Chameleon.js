d3273e21517eb7a9cab7679da24884fe9ec11c46
Tim <timcpb@hotmail.com>
2021-02-02 18:41:36 +0000

    refactor onClick handler

--- react_cam/src/Chameleon.js
+++ react_cam/src/Chameleon.js
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
-import TextComp from './TextComp';
+import TextComp from "./TextComp";
 
 class Chameleon extends Component {
   constructor(props) {
     super(props);
-    this.state = {color: ""}
+    this.state = { color: "" };
 
     // state is a color - red by default
   }
 
   changeColor = (event) => {
-    // on click of button with id of color change the sate to that color 
-    this.setState({color: event.target.value});
-  }
+    // on click of button with id of color change the sate to that color
+    this.setState({ color: event.target.value });
+  };
 
-
-
-  // change color - set state to green or blue or red and is set off when 
+  // change color - set state to green or blue or red and is set off when
   // there is an event handler onclick of one of the buttons
 
   render() {
     return (
       <div className="Chameleon">
-        <TextComp color={this.state.color}/>  
-        <button value="red" onclick={this.changeColor}>Red</button>
-        <button value="green" onclick={this.changeColor}>Green</button>
-        <button value="blue" onclick={this.changeColor}>Blue</button>
+        <TextComp color={this.state.color} />
+        <button value="red" onClick={this.changeColor}>
+          Red
+        </button>
+        <button value="green" onClick={this.changeColor}>
+          Green
+        </button>
+        <button value="blue" onClick={this.changeColor}>
+          Blue
+        </button>
       </div>
     );
   }
